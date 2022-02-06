f69189cf05b8092267b8301724b1901c3042f86a
jordicher <jordi_sodiac@hotmail.com>
2022-02-06 19:19:53 +0100

    add context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,21 +7,26 @@ import Information from '../containers/Information'
 import NotFound from '../containers/NotFound'
 import Payment from '../containers/Payment'
 import Success from '../containers/Success'
+import AppContext from '../context/AppContext'
+import useInitialState from '../hooks/useInitialState'
 
 function App() {
+  const initialState = useInitialState()
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="checkout/information" element={<Information />} />
-          <Route path="checkout/payment" element={<Payment />} />
-          <Route path="checkout/success" element={<Success />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="checkout/information" element={<Information />} />
+            <Route path="checkout/payment" element={<Payment />} />
+            <Route path="checkout/success" element={<Success />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
 
