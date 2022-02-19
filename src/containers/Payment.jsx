a2e179d31eedd722f767255075989091d9cecaa3
jordicher <jordi_sodiac@hotmail.com>
2022-02-19 22:11:51 +0100

    add paypal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PayPalButton } from 'react-paypal-button-v2'
+import { useNavigate } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 
 function Payment() {
+  const navigate = useNavigate()
+
+  const {
+    state: { cart, buyer },
+    addNewOrder
+  } = useContext(AppContext)
+
+  const paypalOtions = {
+    clientId: import.meta.env.CLIENT_ID_PAYPAL,
+    intent: 'capture',
+    currency: 'EUR'
+  }
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect'
+  }
+
+  const handlePaymentSuccess = data => {
+    console.log(data)
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data
+      }
+      addNewOrder(newOrder)
+      navigate('/checkout/success', { replace: true })
+    }
+  }
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price
+    const sum = cart.reduce(reducer, 0)
+    return sum
+  }
+
   return (
     <div>
       <div>
         <h3 className="font-bold">Resumen del pedido:</h3>
-        <div>Boton de pago con Paypal</div>
+        {cart.map(item => (
+          <div key={item.id} className="flex flex-row space-x-5">
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+          </div>
+        ))}
+        <div>
+          <PayPalButton
+            paypalOptions={paypalOtions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal()}
+            onPaymentStart={() => console.log('Start Payment')}
+            onPaymentSuccess={data => handlePaymentSuccess(data)}
+            onPaymentError={error => console.log(error)}
+            onPaymentCancel={data => console.log(data)}
+          />
+        </div>
       </div>
       <div></div>
     </div>
@@ -13,3 +70,5 @@ function Payment() {
 }
 
 export default Payment
+/* access_token$sandbox$592rs4xjb2v2mhs9$9fa27a9618875ea844b9258396c7c551
+ */
--- src/initialState.js
+++ src/initialState.js
@@ -4,42 +4,42 @@ export default {
   products: [
     {
       id: '1',
-      image: 'https://arepa.s3.amazonaws.com/camiseta.png',
+      image: 'https://picsum.photos/200/300',
       title: 'Camiseta',
       price: 25,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     },
     {
       id: '3',
-      image: 'https://arepa.s3.amazonaws.com/mug.png',
+      image: 'https://picsum.photos/600',
       title: 'Mug',
       price: 10,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     },
     {
       id: '4',
-      image: 'https://arepa.s3.amazonaws.com/pin.png',
+      image: 'https://picsum.photos/400',
       title: 'Pin',
       price: 4,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     },
     {
       id: '5',
-      image: 'https://arepa.s3.amazonaws.com/stickers1.png',
+      image: 'https://picsum.photos/500',
       title: 'Stickers',
       price: 2,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     },
     {
       id: '6',
-      image: 'https://arepa.s3.amazonaws.com/stickers2.png',
+      image: 'https://picsum.photos/250',
       title: 'Stickers',
       price: 2,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
     },
     {
       id: '7',
-      image: 'https://arepa.s3.amazonaws.com/hoodie.png',
+      image: 'https://picsum.photos/200',
       title: 'Hoodie',
       price: 35,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
